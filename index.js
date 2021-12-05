60a162e7bb135ed224457e6ae0b2df45bcdcd13c
Mgayar-Sci <dr.mohamedelgayar@gmail.com>
2021-12-05 04:01:06 +0200

    Added mongoose quick start -connected -inserted data -query data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //git init
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,3 +12,33 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// https://mongoosejs.com/docs/
+const mongoose = require('mongoose');
+
+main().then(() => console.log("mongodb is connected")).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://sa:W6Ym9SRNV8j7yhpn@cluster0.upgxv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+  const s_exists = await Kitten.find({ name: /^Silence/ });
+  
+  if (!s_exists) {
+    const silence = new Kitten({ name: 'Silence' });
+    await silence.save();
+  }
+
+  const f_exists = await Kitten.find({ name: /^fluffy/ });
+
+  if (!f_exists) {
+    const fluffy = new Kitten({ name: 'fluffy' });
+    await fluffy.save();
+  }
+  Kitten.find().then(k=>console.log(k));
+}
+
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
