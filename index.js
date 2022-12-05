a64cff29136c0816ab8aac3e40ee7e5d5d1e1322
Mgayar-Sci <dr.mohamedelgayar@gmail.com>
2022-12-05 04:48:52 +0200

    Use cors+body parser+post+body

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 //https://expressjs.com/en/starter/hello-world.html
 //git init
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.json());
 
 app.listen(port, () =>
   console.log(`Server is listening at http://localhost:${port}`)
@@ -22,8 +26,8 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
 
-app.get('/login', (req, res) => {
-  const { email, password } = req.query;
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
 
   if (!email || !password) {
     error = { error: "no email or password" };
@@ -56,8 +60,8 @@ app.get('/list', (req, res) => {
     });
 });
 
-app.get('/register', (req, res) => {
-  const { name, email, password } = req.query;
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.status(401).send({ error: "missing user data" });
   }
